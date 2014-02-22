66c90023fe8931d1259600ab8c98f413d40ade13
Brad <bradleyjacques@hotmail.com>
2014-02-22 17:36:58 +0000

    document Store interface

--- src/main/java/com/brad/dataserver/dao/Store.java
+++ src/main/java/com/brad/dataserver/dao/Store.java
@@ -3,9 +3,20 @@ package com.brad.dataserver.dao;
 
 public interface Store {
 
+	/**
+	 * Persists the value to the store. 
+	 * Does not allow duplicates or null values, but does not throw an exception in such cases.
+	 * @param value
+	 */
 	void store(String value);
 
+	/**
+	 * @return the most recent value added to the store
+	 */
 	String readLastValue();
 
+	/**
+	 * @return the number of values persisted to the store
+	 */
 	int size();
 }
